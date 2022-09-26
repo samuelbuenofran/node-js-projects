03d24762972264c1d944c9ee26ff0b8e65d5c59f
Samuel Bueno Francisco <71886739+samuelbuenofran@users.noreply.github.com>
2022-09-26 15:02:55 -0300

    Update app.js

--- writing-to-txt-file/app.js
+++ writing-to-txt-file/app.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 // Writes to text file:
-// fs.writeFileSync('notes.txt', 'My name is Andrew.');
+// fs.writeFileSync('notes.txt', 'My name is Samuel. サミュエルと申します。');
 
 // Appends information to text file:
-fs.appendFileSync('notes.txt', ' I live in Philadelphia.');
+fs.appendFileSync('notes.txt', ' I live in the countryside. 私は田舎に住んでいます。');
